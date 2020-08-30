203c1f45768057b96f75a7f5437ab5555a7479b8
Susmita Saha <susmita35-1544@diu.edu.bd>
2020-08-30 23:50:49 +0600

    Environment setup done

---
+++ Complain System/Models/ComplainDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Complain_System.Models
+{
+
+    public class ComplainDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+    public ComplainDbContext(DbContextOptions<ComplainDbContext> options) : base(options)
+    {
+
+    }
+    //tables________________
+
+}
+}
